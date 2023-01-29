4da00e1144a7413e933d02d5448e2a347c0bd174
thisiskhandev <hassankhan427726@gmail.com>
2023-01-30 00:47:19 +0500

    Bug fixed

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -1,9 +1,10 @@
 /*  VARIABLES
-*/
+
 var a = "Test";
 var a = "Test 2";
 let b = "Test b"; // intisialize value
 b = "abcd"; // update value
 const c = "CCC";
-c = "DDDD";
+// c = "DDDD";
 // console.log(c);
+*/
