e494c0bc6463630f87483c33c01baef96e7fcb8b
Hassan Khan <hassankhan427726@outlook.com>
2023-01-30 22:08:40 +0500

    Multidimentional Arrays

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -30,3 +30,13 @@ console.log(studentData);
 var fruits = ["apple", "banana", "orange", "papaya"];
 fruits[0] = fruits[0].toUpperCase();
 console.log(fruits.slice(1, 4));
+
+//  Multidimentional Array
+var gradeYears = [
+  [1, "Ali", "Germany"],
+  [2, "Hassan", "Germany"],
+  [3, "Ali", "Germany"],
+  [4, "Ali", "Germany"],
+  [5, "Ali", "Germany"],
+];
+console.log(gradeYears[3][0]);
