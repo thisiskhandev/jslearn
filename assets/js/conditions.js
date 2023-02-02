3a1cb898d2ed1de3aa6045dd49eb9bcd6f9e1ca3
Hassan Khan <hassankhan427726@outlook.com>
2023-02-02 22:01:40 +0500

    Dialog boxes and conditions

--- assets/js/conditions.js
+++ assets/js/conditions.js
@@ -1,8 +1,16 @@
-/*  CONDITIONS
+/*  CONDITIONS IF ELSE
  
 if (val1 > val2) {
   console.log("Yes it is");
 } else {
   console.warn("No!");
+}
+
+let a = 10; // Number
+let b = "10"; // string
+if (a === b) {
+  console.log("YES");
+} else {
+  console.log("Not same!");
 }
 */
